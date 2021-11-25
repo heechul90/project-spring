66dda240956b0be02b317163e9db579bec882394
hclee <hclee@dataeum.kr>
2021-11-25 19:21:31 +0900

    start spring!

---
+++ heech/src/main/java/hellcoding/heech/HeechApplication.java
@@ -0,0 +1,13 @@
+package hellcoding.heech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeechApplication.class, args);
+	}
+
+}
---
+++ heech/src/test/java/hellcoding/heech/HeechApplicationTests.java
@@ -0,0 +1,13 @@
+package hellcoding.heech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
