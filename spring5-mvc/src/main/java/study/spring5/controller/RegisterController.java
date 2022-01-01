252ef1ea8ca28b37f68df714e34c15ec29f881b9
hclee <hclee@dataeum.kr>
2022-01-01 18:33:20 +0900

    /register/step1 추가

--- spring5-mvc/src/main/java/study/spring5/controller/RegisterController.java
+++ spring5-mvc/src/main/java/study/spring5/controller/RegisterController.java
@@ -4,11 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/register")
 public class RegisterController {
 
-    @RequestMapping("/register/step1")
+    @RequestMapping("/step1")
     public String handleStep1() {
-        System.out.println("zzzzzzzzzzzzzz");
         return "register/step1";
+    }
+
+    @RequestMapping("/step2")
+    public String handleStep2() {
+        return "register/step2";
+    }
+
+    @RequestMapping("/step3")
+    public String hendleStep3() {
+        return "register/step3";
     }
 }
