a6745f56228657294ba196a0894c434d74270e25
hclee <hclee@dataeum.kr>
2022-01-30 23:23:40 +0900

    이메일 기억하기 쿠기저장 추가

--- spring5-mvc/src/main/java/study/spring5/controller/LoginController.java
+++ spring5-mvc/src/main/java/study/spring5/controller/LoginController.java
@@ -1,8 +1,8 @@
 package study.spring5.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +10,9 @@ import study.spring5.domain.AuthInfo;
 import study.spring5.error.WrongIdPasswordException;
 import study.spring5.service.AuthService;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -24,12 +26,17 @@ public class LoginController {
     }
 
     @GetMapping
-    public String form(LoginCommand loginCommand) {
+    public String form(LoginCommand loginCommand, @CookieValue(value = "REMEMBER", required = false) Cookie rCookie) {
+        if (rCookie != null) {
+            loginCommand.setEmail((rCookie.getValue()));
+            loginCommand.setRememberEmail(true);
+        }
         return "login/loginForm";
     }
 
     @PostMapping
-    public String submit(LoginCommand loginCommand, Errors errors, HttpSession session) {
+    public String submit(LoginCommand loginCommand, Errors errors, HttpSession session,
+                         HttpServletResponse response) {
         new LoginCommandValidator().validate(loginCommand, errors);
         if (errors.hasErrors()) {
             return "login/loginForm";
@@ -39,6 +46,15 @@ public class LoginController {
             AuthInfo authInfo = authService.authenticate(loginCommand.getEmail(), loginCommand.getPassword());
 
             session.setAttribute("authInfo", authInfo);
+
+            Cookie rememberCookie = new Cookie("REMEMBER", loginCommand.getEmail());
+            rememberCookie.setPath("/");
+            if (loginCommand.isRememberEmail()) {
+                rememberCookie.setMaxAge(60 * 60 * 24 * 30);
+            } else {
+                rememberCookie.setMaxAge(0);
+            }
+            response.addCookie(rememberCookie);
 
             return "login/loginSuccess";
         } catch (WrongIdPasswordException e) {
